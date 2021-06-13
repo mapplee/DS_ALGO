4e0775d09f83530b8c63a650ac70e266b8d2f5a6
mapplee <iapplee007@gmail.com>
2021-06-13 15:44:31 +0600

    Added remove and insert node between two  node

--- link_list.c
+++ link_list.c
@@ -78,18 +78,53 @@ Node *search(Node *head , int item)
 	}
 	return current_node;
 }
+
+Node *remove_node(Node *head, Node *node)
+{
+	if(node == head)
+	{
+		head=node->next;
+		return head;
+	}
+
+	Node *current_node;
+	current_node=head;
+	while(current_node != NULL)
+	{
+		if(current_node->next ==node)
+		break;
+		current_node=current_node->next;
+	}
+	if(current_node == NULL)
+	{
+		return head;
+	}
+	current_node->next=node->next;
+	return head;
+
+}
+void insert(Node *node,int item)
+{
+	Node *new_node=(Node *)malloc(sizeof(Node));
+	new_node=create_node(item,node->next);
+	node->next=new_node;
+}
 int main()
 {
 	Node *head;
 	head=create_node(10,NULL);
+	Node *n1,*n2,*n3;
+	n1=head;
 	//Node *n1;
 	//n1=create_node(5,NULL);
 	print_linked_list(head);
 	printf("Element of link list : %d\n",count(head));
 	head=prepend_node(head,20);
+	n2=head;
 	print_linked_list(head);
 	printf("Element of link list : %d\n",count(head));
 	head=append(head,30);
+	n3=head;
 	print_linked_list(head);
 	printf("Element of link list : %d\n",count(head));
 
@@ -99,9 +134,20 @@ int main()
 	Node *search_item;
 	search_item=search(head,element);
 	if(search_item!=NULL)
-		printf("Item %d found ",search_item->data);
+		printf("Item %d found \n",search_item->data);
 	else
-		printf("Item not found");
+		printf("Item not found\n");
+	printf("Remove Node from linked list: ");
+	//scanf("%d",&item);
+	head=remove_node(head,head);
+	printf("\nHead removed ");
+	print_linked_list(head);
+
+	printf("\nInsert item between two node : ");
+	scanf("%d",&element);
+	insert(head,element);
+	printf("\nInsert item %d between head and n3\n",element);
+	print_linked_list(head);
 //	head=prepend_node(head,20);
 //	print_linked_list(head);
 //	head=append(head,30);
